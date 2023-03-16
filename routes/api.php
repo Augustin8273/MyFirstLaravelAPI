b950eb0c21160abf0638ae4233c1cc2a991ff28b
Augustin8273 <augubu9@gmail.com>
2023-03-16 14:15:58 +0200

    s

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\http\Controllers\PostController;
+use App\http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
